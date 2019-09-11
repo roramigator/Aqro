e69319acb8ef647b4079b1deaab77604244c0f56
roramigator <roramigator@gmail.com>
2019-09-11 08:02:07 -0400

    Creates new record

--- ajax/addRecord.php
+++ ajax/addRecord.php
@@ -1,21 +1,22 @@
 <?php
 	if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']))
 	{
-		// include Database connection file 
+		// include Database connection file
 		include("db_connection.php");
 
-		// get values 
+		// get values
 		$first_name = $_POST['first_name'];
 		$last_name = $_POST['last_name'];
 		$email = $_POST['email'];
-		$sales = $_POST['sales'];		
+		$sales = $_POST['sales'];
 		$price = $_POST['price'];
 		$barcode = $_POST['barcode'];
 
 		$query = "INSERT INTO users(first_name, last_name, email, sales, price, barcode) VALUES('$first_name', '$last_name', '$email', '$sales', '$price', '$barcode')";
+
 		if (!$result = mysqli_query($db, $query)) {
 	        exit(mysqli_error());
 	    }
-	    echo "Registro Agregado!";
+
 	}
 ?>
